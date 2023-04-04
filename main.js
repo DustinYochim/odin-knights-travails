bd1a8ac45a6425a786b934090eca268c13d8f3a6
Dustin Yochim <yochim.dustin@gmail.com>
2023-04-04 12:27:02 -0500

    Add functionality to reach end and output path

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
-function knightMoves(start, end) {}
-
-// Creates 8x8 Chessboard
-function createBoard() {
-  let board = [];
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      board.push([i, j]);
-    }
+function Node(pos, path) {
+  if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) {
+    return null;
   }
-  return board;
+  return { pos, path };
 }
 
-// Generates all moves from current location
-function getMoves(location) {
-  const moves = [];
-  moves.push([location[0] + 1, location[1] - 2]);
-  moves.push([location[0] + 1, location[1] + 2]);
-  moves.push([location[0] - 1, location[1] - 2]);
-  moves.push([location[0] - 1, location[1] + 2]);
-  moves.push([location[0] + 2, location[1] - 1]);
-  moves.push([location[0] + 2, location[1] + 1]);
-  moves.push([location[0] - 2, location[1] - 1]);
-  moves.push([location[0] - 2, location[1] + 1]);
+function knightMoves([x, y], [a, b]) {
+  // Add the starting position node to the queue
+  let q = [Node([x, y], [[x, y]])];
+  let currentNode = q.shift();
+
+  // While the current position != end position
+  while (currentNode.pos[0] !== a || currentNode.pos[1] !== b) {
+    // Generate moves from current position
+    let moves = generateMoves(currentNode);
+    // Add valid moves to the queue
+    moves.forEach((move) => {
+      let node = Node(move, currentNode.path.concat([move]));
+      if (node) {
+        q.push(node);
+      }
+    });
+    // Move to next node in the queue
+    currentNode = q.shift();
+  }
+  // When loop exits, you arrived at destination
+  console.log(
+    `=> You made it in ${currentNode.path.length - 1} moves!  Here's your path:`
+  );
+  console.log(currentNode.path);
+}
+
+function generateMoves(currentNode) {
+  let moves = [
+    [currentNode.pos[0] + 2, currentNode.pos[1] - 1],
+    [currentNode.pos[0] + 2, currentNode.pos[1] + 1],
+    [currentNode.pos[0] - 2, currentNode.pos[1] - 1],
+    [currentNode.pos[0] - 2, currentNode.pos[1] + 1],
+    [currentNode.pos[0] + 1, currentNode.pos[1] - 2],
+    [currentNode.pos[0] + 1, currentNode.pos[1] + 2],
+    [currentNode.pos[0] - 1, currentNode.pos[1] - 2],
+    [currentNode.pos[0] - 1, currentNode.pos[1] + 2],
+  ];
   return moves;
 }
 
-// Returns only moves that are valid on the board
-function checkMoves(moves) {
-  return moves.filter((move) => {
-    return move[0] >= 1 && move[0] <= 8 && move[1] >= 1 && move[1] <= 8;
-  });
-}
-
-console.log(knightMoves([0, 0], [3, 3]));
+knightMoves([0, 0], [3, 3]);
